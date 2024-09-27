b25877ae2738f54b3b14b185e726ba92258d3551
lqiang2003cn <randycoq@163.com>
2024-09-27 17:35:29 +0800

    agent

---
+++ agent/__main__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024 Microsoft Corporation.
+# Licensed under the MIT License
+
+"""The Indexing Engine package root."""
+
+
+import graphrag.index.utils as utils
+
+if __name__ == "__main__":
+    print(utils.uuid.gen_uuid())
